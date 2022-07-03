22a5948614abc5d49878fda0633b07edf11b018c
retip94 <piotr.piekielny@appunite.com>
2022-07-03 19:54:40 +0200

    added test for dollars equality, didnt have to implement equals, because structs with the same values inside are equal (chapter 3)

--- test/money_example_tdd_test.exs
+++ test/money_example_tdd_test.exs
@@ -6,4 +6,8 @@ defmodule MoneyExampleTddTest do
     dollar = %Dollar{amount: 5}
     assert Dollar.times(dollar, 2) == %Dollar{amount: 10}
   end
+
+  test "test dollars equality" do
+    assert %Dollar{amount: 5} == %Dollar{amount: 5}
+  end
 end
